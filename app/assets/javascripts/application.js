473ca0a2a732102851e823b5853a3db51fbcff96
Kevin Rockwood <kevin@rockwood.me>
2013-10-29 22:49:24 -0400

    Remove link scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,5 @@
 //= require jquery
 
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        || location.hostname == this.hostname) {
 
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 300);
-        return false;
-      }
-    }
-  });
 });
